a019ccc3c334d9cabea1946d718f49e7e49acb25
AGNESH K <agnesh9025@gmail.com>
2025-07-25 17:12:30 +0530

    SignUp Method Implemented

--- src/main/java/com/auth/loginauthentication/Service/AuthService.java
+++ src/main/java/com/auth/loginauthentication/Service/AuthService.java
@@ -5,6 +5,7 @@ import com.auth.loginauthentication.DTO.AuthRequest;
 import com.auth.loginauthentication.DTO.AuthResponse;
 import com.auth.loginauthentication.Repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -22,5 +23,15 @@ public class AuthService {
         String  encodedPassword = passwordEncoder.encode(request.getPassword());
         String accessToken = jwtService.generateAccessToken(request.getEmail());
         String refreshToken = jwtService.generateRefreshToken(request.getEmail());
+
+        User user = User.builder()
+                .email(request.getEmail())
+                .password(encodedPassword)
+                .refreshToken(refreshToken)
+                .build();
+        userRepository.save(user);
+        return AuthResponse.builder()
+                .accessToken(accessToken)
+                .build();
     }
 }
